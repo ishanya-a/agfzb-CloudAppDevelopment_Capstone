823136fbf2901d7003695a2d8c355b3f11aff5d4
ishanya-a <ichandwadkar26@gmail.com>
2023-07-12 13:52:35 -0400

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     # path for logout
 
     path(route='', view=views.get_dealerships, name='index'),
+    path(route='about', view=views.about, name='about'),
 
     # path for dealer reviews view
 
